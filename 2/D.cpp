e98629294b68efd86d7ffeacf9d6791ef341adda
Serelllka <44942855+Serelllka@users.noreply.github.com>
2021-09-06 01:21:12 +0300

    Update D.cpp

--- 2/D.cpp
+++ 2/D.cpp
@@ -1,77 +1,29 @@
 #include<iostream>
 #include<vector>
  
-#define pb push_back
- 
 using namespace std;
- 
-long long k = 0;
- 
-void merge (long long l, long long r, vector<long long> &a)
-{
-    if (r - l > 0)
-    {
-        long long m = (l + r) / 2;
-        merge (l, m, a);
-        merge (m + 1, r, a);
-        long long xl = l, xr = m + 1, i = 0;
-        long long mas[a.size() + 1];
-        while (xl <= m && xr <= r)
-        {
-            if (a[xl] <= a[xr])
-            {
-                mas[i] = a[xl];
-                xl++;
-            }
-            else
-            {
-                mas[i] = a[xr];
-                xr++;
-                k += m + 1 - xl;
-            }
-            ++i;
-        }
- 
-        while (xl <= m)
-        {
-            mas[i] = a[xl];
-            i++;
-            xl++;
-        }
- 
-        while (xr <= r)
-        {
-            mas[i] = a[xr];
-            i++;
-            xr++;
-        }
- 
-        for (int j = 0; j < i; ++j)
-        {
-            a[l + j] = mas[j];
-        }
-    }
- 
-}
- 
- 
  
 int main()
 {
-    freopen ("inversions.in", "r", stdin);
-    freopen ("inversions.out", "w", stdout);
+    #ifndef ONPC
+        freopen ("antiqs.in", "r", stdin);
+        freopen ("antiqs.out", "w", stdout);
+    #endif // ONPC
  
-    vector<long long> a;
-    long long n;
+    int n;
     cin >> n;
-    for (long long i = 0; i < n; ++i)
+    int a[n];
+    for (int i = 1; i <= n; ++i)
     {
-        long long x;
-        cin >> x;
-        a.pb(x);
+        a[i - 1] = i;
     }
-    merge (0, a.size() - 1, a);
-    cout << k;
+    for (int i = 0; i < n; i += 1)
+    {
+        swap(a[i], a[i / 2]);
+    }
  
-    return 0;
+    for (int i = 0; i < n; ++i)
+    {
+        cout << a[i] << ' ';
+    }
 }
