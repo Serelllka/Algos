dd8ed8747061e16f119ad68d288e7531a47375e3
Serelllka <44942855+Serelllka@users.noreply.github.com>
2021-09-06 01:13:25 +0300

    Update A.cpp

--- 1/A.cpp
+++ 1/A.cpp
@@ -1,1 +1,17 @@
 #include<iostream>
+#include<fstream>
+ 
+using namespace std;
+ 
+int main()
+{
+    freopen ("aplusb.in", "r", stdin);
+    freopen ("aplusb.out", "w", stdout);
+ 
+    int x, y;
+ 
+    cin >> x >> y;
+    cout << x +  y;
+ 
+    return 0;
+}
